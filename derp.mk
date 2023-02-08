6acd341b96e8a140034e67e5bac423fdb6cd5ae2
TheAttila <Damsem39@gmail.com>
2023-02-08 19:50:07 -0300

    January update

---
+++ derp.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/BoardConfigDerpFest.mk)
+$(call inherit-product, vendor/derp/config/BoardConfigSoong.mk)
+$(call inherit-product, device/derp/sepolicy/common/sepolicy.mk)
+-include vendor/derp/build/core/config.mk
+                           
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+TARGET_NO_KERNEL_OVERRIDE := true                                       
+TARGET_NO_KERNEL_IMAGE := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 720
+USE_LEGACY_BOOTANIMATION := true
+#WITH_GMS := true
+
